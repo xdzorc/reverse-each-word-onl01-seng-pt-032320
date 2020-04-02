7870a70eb0612c62929965bcbdadaa0e6bd95130
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 00:37:51 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word(str)
+  array=str.split(" ")
+  newarray=[]
+  array.each do |x|
+    newarray << x.reverse
+  end
+  newarray.join(" ")
+end
+
+def reverse_each_word(str)
+  array=str.split(" ")
+  newarray=[]
+  array.collect do |x|
+    newarray << x.reverse
+  end
+  newarray.join(" ")
+end
+    
